0680e1cb81deb9fae489aa8c0cc5c87608598b24
Teruyoshi Matsushima <73378281+Teruyoshi-Matsushima@users.noreply.github.com>
2022-12-11 02:41:58 +0900

    first commit

---
+++ additional/newfile.py
@@ -0,0 +1,13 @@
+import os 
+
+from flask import Flask
+
+port = int(os.environ['PORT'])
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return '<h1>Hello World Cup! </h1></br><h2>感動をありがとー！！</h2>'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=81)
